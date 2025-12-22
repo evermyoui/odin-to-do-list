a92bc982fe243a815d9905e39c0d2c53002da892
said <sexbythewind@gmail.com>
2025-12-22 23:09:26 +0800

    initial webpack setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-to-do-list
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { watchFiles } = require("fs");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+            new HTMLWebpackPlugin({
+            template: "./src/template.html"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+};
