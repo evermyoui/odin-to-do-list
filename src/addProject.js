4b2b7e34a6aad65952252d96d572b8304abc235c
said <sexbythewind@gmail.com>
2025-12-25 22:28:58 +0800

    Add Project function and logs project

--- src/addProject.js
+++ src/addProject.js
@@ -3,8 +3,34 @@ export default function addProject(){
     const label = document.querySelector(".project-label");
     const input = document.querySelector("#project-title");
 
+    const project = new Add_Project();
+    let firstTap = true;
+
     addProjectBtn.addEventListener("click", ()=>{
+        if (firstTap){
+            project.displayInput(label, input);
+            firstTap = false;
+        }else {
+            project.addTitle(input.value);
+            project.clear(label, input);
+            firstTap = true;
+        }
+    });
+}
+class Add_Project {
+    displayInput(label, input){
         label.style.display = "block";
         input.style.display = "block";
-    });
+    }
+    addTitle(title){
+        if (!title.trim()){
+            title = `Unnamed Project`;
+        }
+        console.log(`Project ${title} has been saved`);
+    }
+    clear(label, input){
+        label.style.display = "none";
+        input.style.display = "none";
+        input.value = "";
+    }
 }
