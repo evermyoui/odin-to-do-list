c44f46fd105202079d0069b294e8c4247083c4ae
said <sexbythewind@gmail.com>
2025-12-28 23:28:00 +0800

    Collapse to different independent files

--- src/addProject.js
+++ src/addProject.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import Add_Project from "./uiProject";
 
 export default function addProject(){
     const addProjectBtn = document.querySelector(".addProjectButton");
@@ -18,45 +18,4 @@ export default function addProject(){
             firstTap = true;
         }
     });
-}
-class Add_Project {
-    displayInput(label, input){
-        label.style.display = "block";
-        input.style.display = "block";
-    }
-    clear(label, input){
-        label.style.display = "none";
-        input.style.display = "none";
-        input.value = "";
-    }
-    uiDisplay(title){
-        const projectUl = document.querySelector(".project-ul");
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const li = document.createElement("li");
-        if (!title.trim()){
-            title = `Unnamed Project`;
-        }
-        const p = document.createElement("p");
-        p.textContent = title;
-        const p1 = document.createElement("p");
-        const today = new Date();
-        const formatted = format(today, "yyyy-MM-dd");
-        p1.textContent = formatted;
-        const div = document.createElement("div");
-        const addBtn = document.createElement("button");
-        addBtn.classList.add("add-button");
-        addBtn.textContent = "Add To-do";
-        const deleteBtn = document.createElement("button");
-        deleteBtn.classList.add("delete-button");
-        deleteBtn.textContent = "Delete";
-
-        li.appendChild(p);
-        li.appendChild(p1);
-        div.appendChild(addBtn);
-        div.appendChild(deleteBtn);
-        card.appendChild(li);
-        card.appendChild(div);
-        projectUl.appendChild(card);
-    }
 }
