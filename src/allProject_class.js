0070c5b5dc70f9bd8ff576d6b72fbe4d79981d46
said <sexbythewind@gmail.com>
2026-01-05 21:22:40 +0800

    Add Local Storage

--- src/allProject_class.js
+++ src/allProject_class.js
@@ -1,24 +1,22 @@
-class AllProjects{
+import Project from "./project_class";
+export default class AllProjects{
     constructor(){
-        this.allProject = getLocalStorage("allProject") ||[];
+        this.allProject = JSON.parse(localStorage.getItem("allProject")) || [];
         this.deletedProject = []; 
     }
-    addProject(project){
+    addProject(title){
+        const project = new Project(title);
         this.allProject.push(project);
-    }
-    getProject(index){
-        return this.allProject[index];
+        return project;
     }
     deleteProject(projectID){
         const index = this.allProject.findIndex(project => project.id === projectID);
         if (index === -1) return;
         this.deletedProject.push(this.allProject[index]);
-        this.allProject = this.allProject.filter(project => project.id !== projectID);
+        this.allProject.splice(index, 1);
+        settingLocalStorage("allProject", this.allProject);
     }
 }
-function getLocalStorage(key){
-    return JSON.parse(localStorage.getItem(key));
-}
-function settingLocalStorage(key, value){
+export function settingLocalStorage(key, value){
     localStorage.setItem(key, JSON.stringify(value));
 }
