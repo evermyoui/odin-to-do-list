a32921a494ad9f1839b205f0bf9cbb8f02c935b3
said <sexbythewind@gmail.com>
2026-01-06 22:07:03 +0800

    Add cancel button

--- src/addProject.js
+++ src/addProject.js
@@ -5,12 +5,14 @@ export default function addProject(){
     const addProjectBtn = document.querySelector(".addProjectButton");
     const label = document.querySelector(".project-label");
     const input = document.querySelector("#project-title");
+    const cancelBtn = createCancelBtn();
 
     let firstTap = true;
 
     const displayInput = (label, input)=>{
         label.style.display = "block";
         input.style.display = "block";
+        addProjectBtn.parentElement.appendChild(cancelBtn);
     }
     const clear = (label, input) => {
         label.style.display = "none";
@@ -22,10 +24,25 @@ export default function addProject(){
         if (firstTap){
             displayInput(label, input);
             firstTap = false;
+            firstTap ? cancelBtn.style.display = "none" : cancelBtn.style.display = "block";
         }else {
             uiDisplay(input.value);
             clear(label, input);
             firstTap = true;
+            firstTap ? cancelBtn.style.display = "none" : cancelBtn.style.display = "block";
         }
     });
+
+    cancelBtn.addEventListener("click", ()=>{
+        clear(label, input);
+        firstTap = true;
+        firstTap ? cancelBtn.style.display = "none" : cancelBtn.style.display = "block";
+    });
+    
+}
+function createCancelBtn(){
+    const cancelBtn = document.createElement("button");
+    cancelBtn.classList.add("cancel");
+    cancelBtn.textContent = "Cancel";
+    return cancelBtn;
 }
