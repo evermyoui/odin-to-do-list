7362563a17bd0c17c0cf46538b6ab620ae3d2a0e
said <sexbythewind@gmail.com>
2026-02-12 20:19:50 +0800

    add date creation

--- src/factories/project.js
+++ src/factories/project.js
@@ -1,7 +1,10 @@
+import { format } from "date-fns";
+
 export class Project {
     constructor(title, ){
         this.title = title;
         this.todos = [];
         this.id = crypto.randomUUID();
+        this.dateCreated = format(new Date(), 'dd-MMM-yyyy');
     }
 }
