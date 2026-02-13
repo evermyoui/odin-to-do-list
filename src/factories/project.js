9cd76395aaf6b4c27550fcafb24df797b64272d0
said <sexbythewind@gmail.com>
2026-02-13 16:07:37 +0800

    add falsy unnamed project

--- src/factories/project.js
+++ src/factories/project.js
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 export class Project {
     constructor(title, ){
-        this.title = title;
+        this.title = title.trim() || "Unnamed Project";
         this.todos = [];
         this.id = crypto.randomUUID();
         this.dateCreated = format(new Date(), 'dd-MMM-yyyy');
