81eef305a9f96ffde2833528fbba775ffc7f6e0d
said <sexbythewind@gmail.com>
2026-02-13 16:08:22 +0800

    Run main app

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import {renderTemplate} from "./template.js"
-import { ProjectManager } from "./dom/project_manager";
-import { TodoManager } from "./dom/todo_manager";
-import { AddProjectUI } from "./dom/addProjectUI";
-import fetchQuote from "./quoteRandomizer";
-import "./style.css";
+import { Main_App } from "./app/main_app";
 
-function init(){
-    renderTemplate();
-
-    const projectManager = new ProjectManager();
-    const todoManager = new TodoManager(projectManager);
-    const addProjectUI = new AddProjectUI(projectManager);
-
-    window.todoManager = todoManager;
-
-    fetchQuote();
-}
-
-if (document.readyState === 'loading'){
-    document.addEventListener('DOMContentLoaded', init);
-}else {
-    init();
-}
+const app = new Main_App();
+app.homePage();
