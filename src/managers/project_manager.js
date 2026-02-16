267c56961fef7d6a3741c16a220d9850f7383dae
said <sexbythewind@gmail.com>
2026-02-16 18:54:22 +0800

    add delete Project

--- src/managers/project_manager.js
+++ src/managers/project_manager.js
@@ -16,6 +16,12 @@ export const createProject = (title) => {
     return project;
 }
 
+export const deleteProject = (projectId) => {
+    const index = projectDependencies.projects.findIndex(project => project.id === projectId);
+    if (index ===-1) return;
+    projectDependencies.projects.splice(index,1);
+}
+
 export const addTodoToProject = (projId, todo) => {
     const project = projectDependencies.projects.find(p => p.id === projId);
     if (!project) return;
