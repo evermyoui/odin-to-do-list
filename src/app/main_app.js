a646fa28e59f5ab4b4d70f957b01c2f5aeec451f
said <sexbythewind@gmail.com>
2026-02-16 18:51:25 +0800

    adding project to ui

--- src/app/main_app.js
+++ src/app/main_app.js
@@ -1,8 +1,8 @@
-import { is } from "date-fns/locale";
 import { 
     displayCard, 
     displayEnterProject, 
 } from "../dom/display_dom";
+import { createProject, projectDependencies } from "../managers/project_manager";
 
 export class Main_App {
     constructor(){   
@@ -10,32 +10,38 @@ export class Main_App {
     homePage(){
         displayCard();
         displayEnterProject();
-        addAndCancelButtons();
+        this.addAndCancelButtons();
     }
-}
-function addAndCancelButtons(){
-    let isConfirm = false;
+    addAndCancelButtons(){
+        let isConfirm = false;
 
-    const addBtn = document.querySelector('#project-add-btn');
-    const cancelBtn = document.querySelector('#cancel-add-project-btn');
-    const projInput = document.querySelector("#project-input-title");
-    addBtn.addEventListener("click" ,()=> {
-        if (!isConfirm){
-            cancelBtn.style.display = `block`;
-            projInput.style.display = `block`;
-            addBtn.textContent = "Confirm Add Project";
-            isConfirm = true;
-        }else {
-            console.log("Hi from confirm");
+        const addBtn = document.querySelector('#project-add-btn');
+        const cancelBtn = document.querySelector('#cancel-add-project-btn');
+        const projInput = document.querySelector("#project-input-title");
+        const container = document.querySelector(".container");
+
+        addBtn.addEventListener("click" ,()=> {
+            if (!isConfirm){
+                cancelBtn.style.display = `block`;
+                projInput.style.display = `block`;
+                addBtn.textContent = "Confirm Add Project";
+                isConfirm = true;
+            }else {
+                createProject(projInput.value);
+                container.innerHTML = '';
+                this.homePage();
+                console.log(projectDependencies);
+                cancelBtn.style.display = `none`;
+                projInput.style.display = `none`;
+                isConfirm = false;
+                addBtn.textContent = "Add Project";
+            }
+        });
+        cancelBtn.addEventListener("click", ()=> {
             cancelBtn.style.display = `none`;
             projInput.style.display = `none`;
+            addBtn.textContent = `Add Project`;
             isConfirm = false;
-        }
-    });
-    cancelBtn.addEventListener("click", ()=> {
-        cancelBtn.style.display = `none`;
-        projInput.style.display = `none`;
-        addBtn.textContent = `Add Project`;
-        isConfirm = false;
-    })
+        })
+    }
 }
