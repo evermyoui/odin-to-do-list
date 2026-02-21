3c073029ba2e193e41d073089f9d00222e784b3a
said <sexbythewind@gmail.com>
2026-02-21 12:11:23 +0800

    Move functions in projectDependencies

--- src/managers/project_manager.js
+++ src/managers/project_manager.js
@@ -1,26 +1,61 @@
 import { Project } from "../factories/project";
+import { Storage } from "../factories/storage";
+
+const storage = new Storage();
 
 export const projectDependencies = (() => {
-    const defaultProject = new Project("default");
-    const projects = [defaultProject];
+    let projects = [];
+
+    const init = () => {
+        const saved = storage.load();
+
+        if (saved.length = 0){
+            const defaultProject = new Project("default");
+            const projects = [defaultProject];
+            storage.save(projects);
+        }else {
+            projects.map(p => {
+                const project = new Project(p.title);
+                project.id = p.id;
+                project.todos = p.todos || [];
+                project.dateCreated = p.dateCreated;
+                return project;
+            })
+        }
+    }
+    const getProjects = ()=> projects;
+
+    const createProject = (title) => {
+        const project = new Project(title);
+        projects.push(project);
+        storage.save(projects);
+        return project;
+    }
+
+    const deleteProject = (projectId) => {
+        if (projectId === projects[0].id) return;
+        const index = projects.findIndex(project => project.id === projectId);
+        if (index ===-1) return;
+        projects.splice(index,1);
+        storage.save(projects);
+    }
+
+    const addTodoToProject = (projId, todo) => {
+        const project = projects.find(p => p.id === projId);
+        if (!project) return;
+        project.todos.push(todo);
+        storage.save(projects);
+        return project;
+    }
 
     return {
-        defaultProject,
-        projects
+        init,
+        getProjects,
+        createProject,
+        deleteProject,
+
     }
 })();
-
-export const createProject = (title) => {
-    const project = new Project(title);
-    projectDependencies.projects.push(project);
-    return project;
-}
-
-export const deleteProject = (projectId) => {
-    const index = projectDependencies.projects.findIndex(project => project.id === projectId);
-    if (index ===-1) return;
-    projectDependencies.projects.splice(index,1);
-}
 
 export const addTodoToProject = (projId, todo) => {
     const project = projectDependencies.projects.find(p => p.id === projId);
