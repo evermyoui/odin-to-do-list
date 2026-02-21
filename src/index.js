dfd053ad6fb665d99fe98a7b6c156b46a530e6d4
said <sexbythewind@gmail.com>
2026-02-21 13:06:16 +0800

    place init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Main_App } from "./app/main_app";
+import { projectDependencies } from "./managers/project_manager";
 
+projectDependencies.init();
 const app = new Main_App();
 app.homePage();
