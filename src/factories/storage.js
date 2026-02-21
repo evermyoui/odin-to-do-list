03713e8e3797a97153c48391fd084d65749f754c
said <sexbythewind@gmail.com>
2026-02-21 12:02:06 +0800

    create Storage class

---
+++ src/factories/storage.js
@@ -0,0 +1,25 @@
+import { projectDependencies } from "../managers/project_manager";
+
+export class Storage {
+    storageAvailable(type){
+        try {
+            const storage = window[type];
+            const x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }catch(e){
+            return false;
+        }
+    }
+    save(projects){
+        if (this.storageAvailable("localStorage")){
+            localStorage.setItem("projects", JSON.stringify(projects));
+        }
+    }
+    load(){
+        if (!this.storageAvailable("localStorage")) return [];
+        const data = localStorage.getItem("projects");
+        return data ? JSON.parse(data) : [];
+    }
+}
