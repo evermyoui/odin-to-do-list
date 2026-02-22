bc1112d4777c51385a759c5c5f2654a8d068f8e1
said <sexbythewind@gmail.com>
2026-02-22 21:04:08 +0800

    Add style css to add hidden toggle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Main_App } from "./app/main_app";
 import { projectDependencies } from "./managers/project_manager";
+import "./style.css";
 
 projectDependencies.init();
 const app = new Main_App();
